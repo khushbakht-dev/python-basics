de1caf8a21a671d2c6e27f6c0e99bc12512e4230
khushbakht-dev <khushbakht.dev1@gmail.com>
2026-05-24 03:10:51 +0500

    Add files via upload

---
+++ stringspractice1.py
@@ -0,0 +1,12 @@
+import math
+print(math.ceil(4.3))
+print(math.floor(4.3))
+x = 10 + 3*2
+print(x)
+y = (2 + 3) * 10 - 3
+print(y)
+a = 2.9
+print(round(a))
+print(abs(-3.3))
+print(10%2)
+print(math.fmod(10,2))
