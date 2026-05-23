215c76949352fa68fd6863e1cc32f56f50a26ce9
khushbakht-dev <khushbakht.dev1@gmail.com>
2026-05-24 03:08:01 +0500

    Add files via upload
    strings practice

---
+++ coolstuffwithstrings.py
@@ -0,0 +1,31 @@
+name = 'Khushbakht is a good girl'
+print(len(name))
+print(name.upper())
+print(name)
+print(name.lower())
+print(name.find('akht'))
+print(name.replace('k', 'j'))
+print('good' in name)
+print('bad' in name)
+print(name.find('good'))
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.find('good'))
+print(name.replace('good', 'bad'))
+print('girl' in name)
+print(10+3)
+print(10-4)
+print(10*5)
+print(10/2)
+print(10//2)
+print(10%2)
+print(10%3)
+print(10**2)
+x = 5
+x*= 5
+print(x)
+
+
+
